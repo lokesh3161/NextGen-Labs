98089cfde58e6558d8cc2ba900ad6d2e4c34cef5
Lokesh Thanala <thanalalokesh5@gmail.com>
2026-04-12 21:12:38 +0530

    Add file upload option to admin panel and update config example

--- CLOUD-DELIVERY/config.example.js
+++ CLOUD-DELIVERY/config.example.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    SHEET_URL: "YOUR_GOOGLE_APPS_SCRIPT_URL_HERE"
+    SHEET_URL: "https://script.google.com/macros/s/AKfycbzHJeiB32DNvBNv28_0-L1vDQYM0sJnk7puEG1SvwOkW2sYs2CnNGZvNmz6cAE5g5g/exec"
 };
